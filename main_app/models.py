2c3e92683b8b454e190befab6f9634378a3b36a1
nadewd <nadewbdemissew@gmail.com>
2023-09-14 10:45:34 -0700

    Added styling, about/all pages, and models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    team = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+
+# Changing this instance method
+# does not impact the database, therefore
+# no makemigrations is necessary
+def __str__self(self):
+    return f'{self.name} ({self.id})'
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    # Include an .html file extension - unlike when rendering EJS templates
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def players_index(request):
+    # We pass data to a template the same way we did in Express
+    return render(request, 'players/index.html', {
+        'players': players
+    })
+
